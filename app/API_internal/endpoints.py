b61c2d44d1a0c6b006abff011176a9bb63dfa988
Sp0kin0ki <sv397733git@gmail.com>
2025-07-20 20:06:38 +0300

    'new endpoints

--- app/API_internal/endpoints.py
+++ app/API_internal/endpoints.py
@@ -88,3 +88,43 @@ async def get_airports(conn = Depends(get_db)):
         }
         for row in results
     ]
+    
+    
+@router.get("/delay_histogram")
+async def delay_histogram(conn = Depends(get_db)):
+    results = await conn.fetch("""
+        SELECT
+            COUNT(*) FILTER (WHERE EXTRACT(EPOCH FROM (fact_departure - plan_departure)) <= 600) AS "0-10 минут",
+            COUNT(*) FILTER (WHERE EXTRACT(EPOCH FROM (fact_departure - plan_departure)) > 600 AND EXTRACT(EPOCH FROM ((fact_departure - plan_departure))) <= 1200) AS "11-20 минут",
+            COUNT(*) FILTER (WHERE EXTRACT(EPOCH FROM (fact_departure - plan_departure)) > 1200 AND EXTRACT(EPOCH FROM (fact_departure - plan_departure)) <= 1800) AS "21-30 минут",
+            COUNT(*) FILTER (WHERE EXTRACT(EPOCH FROM (fact_departure - plan_departure)) > 1800 AND EXTRACT(EPOCH FROM (fact_departure - plan_departure)) <= 7200) AS "31-120 минут",
+            COUNT(*) FILTER (WHERE EXTRACT(EPOCH FROM (fact_departure - plan_departure)) > 7200) AS ">120 минут"
+        FROM flights;
+                        """)
+    
+    return [
+        {
+            **dict(row)
+        }
+        for row in results
+    ]
+    
+    
+@router.get("/cancellations_distribution")
+async def get_cancellations_distribution(conn = Depends(get_db)):
+    results = await conn.fetch("""
+        SELECT 
+            a.name AS airlines,
+            COUNT(*) FILTER (WHERE f.fact_departure IS NULL) AS cancellations
+        FROM flights f
+        JOIN airlines a ON f.iata_code = a.iata_code
+        GROUP BY a.name
+        ORDER BY cancellations DESC;
+                               """)
+    
+    return [
+        {
+            **dict(row)
+        }
+        for row in results
+    ]
